6b3c59a1e73b6d432edea4a01f4736ad973500da
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-02-26 19:20:35 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,19 +4,30 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value <= 1) {
-    return false;
-  }
-  for (int i = 2; i <= value / 2; ++i) {
-    if (value % i == 0) {
-      return false;
+    if (value <= 1) {
+        return false;
     }
-  }
-  return true;
+   for (int i = 2; i <= value / 2; ++i) {
+       if (value <= 1) {
+           return false;
+       }
+   if ((value % i == 0)) {
+       return false;
+   }
+   }
+   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t count = 0;
+    uint64_t num = 2;
+    while (count < n) {
+        if (checkPrime(num)) {
+            ++count;
+        }
+        ++num;
+    }
+    return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
